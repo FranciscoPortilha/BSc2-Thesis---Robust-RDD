c4ef08fc1a688848528d2cd1d088fadd6849e4ce
Francisco <portilhafr@gmail.com>
2024-06-12 06:47:44 +0200

    development on dev files

---
+++ dev.py
@@ -0,0 +1,17 @@
+import methods.sample as smp
+import methods.rrdd as rrdd
+import methods.simulation as sim
+
+sample = smp.genSample('Basic Linear',250,tau=0.3,alpha=-0.15,beta=1,L=200,
+                   outlier=False,outlierMethod='Simple Outside', nOutliers=5,printPlot=False)
+
+res = rrdd.jointFitRD('Robust Huber',sample)
+print(rrdd.splitFitRD('Robust Huber',sample))
+
+
+print(sample)
+print(res.summary())
+
+
+s = sim.simulation(10,'Basic Linear',30,tau=2,alpha=-1,beta=1,outlier=False)
+print(s)
