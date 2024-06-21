a70a514057f5a2a26f8bbb31cfe3d08c78e747b4
Francisco <portilhafr@gmail.com>
2024-06-21 22:09:07 +0200

    final mainSim

--- mainSim.py
+++ mainSim.py
@@ -6,16 +6,16 @@ outlierScenarios = (
     "Small Outside Right",
     2,
     "Outside Right",
-    3,
-    "Oposite Outside",
-    3,
-    "Oposite Inside",
-    3,
-    "Symetric Inside",
     2,
+    "Oposite Outside",
+    2,
+    "Oposite Inside",
+    2,
+    "Symetric Inside",
+    1,
 )
 
-np.random.seed(234567)
+np.random.seed(123456)
 for t in [-0.5,0]:
     simulations(
         r,
