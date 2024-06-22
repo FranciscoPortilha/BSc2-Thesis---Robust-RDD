a4bc48ccd007e69f054a93f48634abf93c1a9b05
Francisco <portilhafr@gmail.com>
2024-06-22 21:20:05 +0200

    work on data extraction for application

--- mainApplication.py
+++ mainApplication.py
@@ -4,70 +4,30 @@ from src.sample import genT
 from src.rrdd import jointFitRD
 import rdrobust
 
-#df_83 = pd.read_csv("application/LinkCO83USden.dat", header=None)
-#df_84 = pd.read_csv("application/LinkCO84USden.dat", header=None)
-# df_85 = pd.read_csv("application/LinkCO85USden.dat", header=None)
-# df_86 = pd.read_csv("application/LinkCO86USden.dat", header=None)
-# df_87 = pd.read_csv("application/LinkCO87USden.dat", header=None)
-# df_88 = pd.read_csv("application/LinkCO88USden.dat", header=None)
-# df_89 = pd.read_csv("application/LinkCO89USden.dat", header=None)
-# df_90 = pd.read_csv("application/LinkCO90USden.dat", header=None)
-# df_90 = pd.read_csv("application/LinkCO90USden.dat", header=None)
-# df_91 = pd.read_csv("application/LinkCO91USden.dat", header=None)
-# df_95 = pd.read_csv("application/LinkCO95USden.dat", header=None)
-# df_96 = pd.read_csv("application/LinkCO96USden.dat", header=None)
-# df_97 = pd.read_csv("application/LinkCO97USden.dat", header=None)
-# df_98 = pd.read_csv("application/LinkCO98USden.dat", header=None)
-# df_99 = pd.read_csv("application/LinkCO99USden.dat", header=None)
-# df_00 = pd.read_csv("application/LinkCO00USden.dat", header=None)
-# df_01 = pd.read_csv("application/LinkCO01USden.dat", header=None)
-df_02 = pd.read_csv("application/LinkCO02USDEN.dat", header=None)
+sample = pd.read_csv("application/data.csv")
 
-Deaths = {}
-Weights = {}
+Y_avgs = {}
+X = {}
+for i in range(1415,1586):
+    Y_avgs = np.append(Y_avgs, sample.loc[sample.X==i].mean().iloc[1])
+    X = np.append(X, i)
 
-#print(df_83.iloc[1][0][0:50])
-#print(df_83.iloc[1][0][42:46])
-# df_85, df_86, df_87, df_88, df_89, df_90, df_91, df_02:
-#for df in df_83,df_84:
-#    for i in range(len(df)):
-#        # weight = int(df_83.iloc[i][0][80:84])
-#        weight = int(df.iloc[i][0][42:46])
-#        death = int(df.iloc[i][0][0])
-#        if (1415 <= weight) & (weight <= 1585):
-#            Weights = np.append(Weights, weight)
-#            if death == 3:
-#                Deaths = np.append(Deaths, 0)
-#            else:
-#                Deaths = np.append(Deaths, 1)
-
-for i in range(len(df_02)):
-    weight = int(df_02.iloc[i][0][80:84])
-    death = int(df_02.iloc[i][0][0])
-    if (1415 <= weight) & (weight <= 1585):
-        Weights = np.append(Weights, weight)
-        if death == 2:
-            Deaths = np.append(Deaths, 0)
-        else:
-            Deaths = np.append(Deaths, 1)
-
-
-Deaths = np.delete(Deaths, 0)
-Weights = np.delete(Weights, 0)
-
+Y_avgs = np.delete(Y_avgs,0)
+X = np.delete(X,0)
 sample = pd.DataFrame(
     {
-        "Y": Deaths,
-        "X": Weights,
-        "Treatment": genT(Weights,1500,False),
+        "Y": Y_avgs,
+        "X": X,
+        "Treatment": genT(X,1500,False),
     }
 )
 sample.Y = sample.Y.astype(float)
 sample.X = sample.X.astype(float)
 sample.Treatment = sample.Treatment.astype(float)
 
+
 print(jointFitRD("OLS", sample, cutoff=1500, b=85, outliers=False).summary())
-print(jointFitRD("Donut", sample, cutoff=1500, b=85, outliers=False,donut=2).summary())
+print(jointFitRD("Donut", sample, cutoff=1500, b=85, outliers=False,donut=3).summary())
 print(jointFitRD("Robust Huber", sample, cutoff=1500, b=85, outliers=False).summary())
 print(jointFitRD("Robust Tukey", sample, cutoff=1500, b=85, outliers=False).summary())
 
